8113b9d837e00752e8fa385401fe60005a08e95c
NaimYoussouf <naim.youssouf@etu.univ-amu.fr>
2019-03-22 17:40:47 +0100

    Modif des Classes Game Objet Pelle Personnage

--- src/main/Objet.java
+++ src/main/Objet.java
@@ -5,8 +5,11 @@ public abstract class Objet {
 	protected String typeObjet;
 	private boolean estRamasse = false;
 	
+	private float x1,x2,y1,y2;
+	
 	public Objet(){
 	}
+	
 
 	public void estRamasse(){
 		estRamasse = true;
--- src/main/Sac.java
+++ src/main/Sac.java
@@ -2,9 +2,22 @@ package main;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Sac extends ArrayList<Objet> {
 	
-	public Sac (){}
+	private Image image;
+	
+	public Sac () throws SlickException{
+		
+		System.out.println("Recupération de l'image des outils");
+		image = new Image("tuiles/365214serie.png");
+		if(image != null)
+			System.out.println("image is not null");
+
+
+	}
 	
 	
 
